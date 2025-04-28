fc439f1f49efa74619bc30c2b2e587df04ec2dfc
RamziAhmar <ramziahmar@gmail.com>
2025-04-28 16:39:29 +0700

    rev

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index() {
-        return view('layouts.main', [
+        return view('main', [
             'page' => 'dashboard',
             'title' => 'Dashboard',
         ]);
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index() {
-        return view('layouts.main', [
+        return view('main', [
             'title' => 'Users',
             'page' => 'users',
         ]);
